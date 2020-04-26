b5387aebc7ef23eb6d9bb9370bd358adba812be6
Jakub Tymejczyk <jakub.tymejczyk@enigma.com.pl>
2020-04-26 09:24:32 +0200

    Input file i timeit

--- jtbrain.py
+++ jtbrain.py
@@ -1,25 +1,33 @@
 import argparse
 import sys
+import timeit
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="brainfuck file with code")
+parser.add_argument("-i", "--input", help="brainfuck file with code")
 args = parser.parse_args()
 
 class BrainfuckInterpreter():
     BRAINFUCK_CHARSER = "><+-.,[]"
     BRAINFUCK_DEF_MEMORY = 8
     running = True
-    memory = [0 for _ in range(BRAINFUCK_DEF_MEMORY)]
+    memory = None
     loops = []
     code_ptr = 0
     mem_ptr = 0
     code = None
+    input_data = None
 
-    def __init__(self, file):
+    def __init__(self, file, input_data = None, ram = None):
         with open(file, "r") as f:
             self.code = f.read()
+        if input_data:
+            self.input_data = open(input_data, "r")
+        else:
+            self.input_data = sys.stdin
         self.strip_code()
         self.loops = [-1 for _ in self.code]
+        self.memory = [0 for _ in range(ram if ram else self.BRAINFUCK_DEF_MEMORY)]
 
     def strip_code(self):
         self.code = [c for c in self.code if c in self.BRAINFUCK_CHARSER]
@@ -103,7 +111,7 @@ class BrainfuckInterpreter():
                 sys.stdout.write("%c" % chr(self.memory[self.mem_ptr]))
                 self.code_ptr_inc()
             elif self.code[self.code_ptr] == ',':
-                self.memory[self.mem_ptr] = ord(sys.stdin.read(1))
+                self.memory[self.mem_ptr] = ord(self.input_data.read(1))
                 self.code_ptr_inc()
             elif self.code[self.code_ptr] == '[':
                 self.code_ptr_set(self.matching_close()) if self.memory_is_zero() else self.code_ptr_inc()
@@ -113,7 +121,10 @@ class BrainfuckInterpreter():
                 self.code_ptr_inc()
             #wait = input()
 
-bf = BrainfuckInterpreter(args.file)
-bf.exec()
+bf = BrainfuckInterpreter(file=args.file, input_data=args.input)
+print("Elapsed: %fs" % timeit.Timer(bf.exec).timeit())
+
+bf = BrainfuckInterpreter(file=args.file, input_data=args.input, ram=1024)
+print("Elapsed: %fs" % timeit.Timer(bf.exec).timeit())
 
 print("")
